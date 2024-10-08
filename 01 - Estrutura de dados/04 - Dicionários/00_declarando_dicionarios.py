ef761c5b13547c2d400827d8b078706e7687ab50
Bruno Rossim <bruno.rossim@hotmail.com>
2024-10-07 21:35:29 -0300

    inserindo comentários nos modulos

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -1,8 +1,8 @@
-pessoa = {"nome": "Guilherme", "idade": 28}
+pessoa = {"nome": "Guilherme", "idade": 28}# declarando com chaves
 print(pessoa)
 
-pessoa = dict(nome="Guilherme", idade=28)
+pessoa = dict(nome="Guilherme", idade=28)#declarando com dict
 print(pessoa)
 
-pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+pessoa["telefone"] = "3333-1234"  # adcionando novo valor ao dicionário {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
 print(pessoa)
--- 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
+++ 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -1,14 +1,17 @@
+#declarando a classe para o programa da bicicletaria
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor):
-        self.cor = cor
-        self.modelo = modelo
+    
+    def __init__(self, cor, modelo, ano, valor): #inicializando as carateristicas da bicicleta definindo um metodo construtor.
+        self.cor = cor#self referencia explixita ao objeto
+        self.modelo = modelo#self. igual a atributo da clase
         self.ano = ano
         self.valor = valor
 
+#definindo metodos, são funções dentro de uma classe.
     def buzinar(self):
         print("Plim plim...")
 
-    def parar(self):
+    def parar(self):#para declarar o metodo, def dou nome ao metodo e defino um argumento, neste caso os self.
         print("Parando bicicleta...")
         print("Bicicleta parada!")
 
@@ -23,7 +26,7 @@ b1 = Bicicleta("vermelha", "caloi", 2022, 600)
 b1.buzinar()
 b1.correr()
 b1.parar()
-print(b1.cor, b1.modelo, b1.ano, b1.valor)
+print(b1.cor, b1.modelo, b1.ano, b1.valor)#exibindo os atributos da classe
 
 b2 = Bicicleta("verde", "monark", 2000, 189)
 print(b2)
